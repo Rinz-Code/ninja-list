88312e1dc06ea1b9a15a2fbbeaccbc82aec3b753
Rinz-Code <rinshinsmails123@gmail.com>
2021-03-25 17:27:29 +0530

    Intial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const About = () =>{
+    return (
+    <>
+        <Head>
+            <title>Ninja List | About</title>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor djj jjssjsjsjj jjjxjjsjjsjsj jjjsjjajsj</p>
+            <p>Lorem ipsum dolor djj jjssjsjsjj jjjxjjsjjsjsj jjjsjjajsj</p>
+        </div>
+    </>
+    );
+}
+
+export default About;
